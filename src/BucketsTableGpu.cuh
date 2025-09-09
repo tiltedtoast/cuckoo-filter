bd47fbf0a4713fb07c1bce0019b94d568a71925e
Tim Dortmann <git@tdortman.com>
2025-09-09 16:00:20 +0200

    hopefully this is now a working, lock-free version

--- src/BucketsTableGpu.cuh
+++ src/BucketsTableGpu.cuh
@@ -11,20 +11,6 @@
 #include <vector>
 #include "common.cuh"
 
-struct SpinLock {
-    int lock_flag = 0;
-
-    __device__ void lock() {
-        while (atomicCAS(&lock_flag, 0, 1) != 0) {
-            // Spin until lock is acquired
-        }
-    }
-
-    __device__ void unlock() {
-        atomicExch(&lock_flag, 0);
-    }
-};
-
 template <size_t numBuckets>
 __global__ void
 initRandStatesKernel(curandState* states, unsigned long long seed) {
@@ -119,22 +105,6 @@ class BucketsTableGpu {
     struct __align__(alignof(TagType)) Bucket {
         TagType tags[bucketSize];
 
-        __device__ int findEmptySlotWarp() const {
-            unsigned int lane_id = threadIdx.x & 31;
-            bool has_empty = false;
-
-            if (lane_id < bucketSize) {
-                has_empty = (tags[lane_id] == EMPTY);
-            }
-
-            unsigned int mask = __ballot_sync(0xffffffff, has_empty);
-            if (mask == 0) {
-                return -1;
-            }
-
-            return __ffs(mask) - 1;
-        }
-
         __device__ bool containsWarp(TagType tag) const {
             unsigned int lane_id = threadIdx.x & 31;
             bool found = false;
@@ -142,18 +112,7 @@ class BucketsTableGpu {
             if (lane_id < bucketSize) {
                 found = (tags[lane_id] == tag);
             }
-
-            unsigned int mask = __ballot_sync(0xffffffff, found);
-            return mask != 0;
-        }
-
-        __device__ int findEmptySlot() const {
-            for (size_t i = 0; i < bucketSize; ++i) {
-                if (tags[i] == EMPTY) {
-                    return static_cast<int>(i);
-                }
-            }
-            return -1;
+            return __ballot_sync(0xffffffff, found) != 0;
         }
 
         __device__ bool contains(TagType tag) const {
@@ -169,31 +128,14 @@ class BucketsTableGpu {
             tags[slot] = tag;
         }
 
-        __device__ bool remove(TagType tag) {
-            for (size_t i = 0; i < bucketSize; ++i) {
-                if (tags[i] == tag) {
-                    tags[i] = EMPTY;
-                    return true;
-                }
-            }
-            return false;
-        }
-
         __device__ TagType getTagAt(size_t slot) const {
             return tags[slot];
         }
     };
 
-    struct InsertionCandidate {
-        size_t bucket_idx;
-        TagType fingerprint;
-    };
-
    private:
     Bucket* d_buckets;
-    SpinLock* d_locks{};
     curandState* d_rand_states{};
-
     size_t* d_numOccupied{};
     size_t h_numOccupied = 0;
 
@@ -226,10 +168,7 @@ class BucketsTableGpu {
    public:
     explicit BucketsTableGpu() {
         CUDA_CALL(cudaMalloc(&d_buckets, numBuckets * sizeof(Bucket)));
-        CUDA_CALL(cudaMalloc(&d_locks, numBuckets * sizeof(SpinLock)));
-        CUDA_CALL(
-            cudaMalloc(&d_numOccupied, sizeof(cuda::std::atomic<size_t>))
-        );
+        CUDA_CALL(cudaMalloc(&d_numOccupied, sizeof(size_t)));
         CUDA_CALL(cudaMalloc(&d_rand_states, numBuckets * sizeof(curandState)));
 
         size_t numBlocks = (numBuckets + blockSize - 1) / blockSize;
@@ -243,9 +182,6 @@ class BucketsTableGpu {
     ~BucketsTableGpu() {
         if (d_buckets) {
             CUDA_CALL(cudaFree(d_buckets));
-        }
-        if (d_locks) {
-            CUDA_CALL(cudaFree(d_locks));
         }
         if (d_numOccupied) {
             CUDA_CALL(cudaFree(d_numOccupied));
@@ -371,12 +307,10 @@ class BucketsTableGpu {
         CUDA_CALL(cudaFree(d_output));
         return h_output;
     }
+
     void clear() {
         CUDA_CALL(cudaMemset(d_buckets, 0, numBuckets * sizeof(Bucket)));
-        CUDA_CALL(cudaMemset(d_locks, 0, numBuckets * sizeof(SpinLock)));
-        CUDA_CALL(
-            cudaMemset(d_numOccupied, 0, sizeof(cuda::std::atomic<size_t>))
-        );
+        CUDA_CALL(cudaMemset(d_numOccupied, 0, sizeof(size_t)));
         h_numOccupied = 0;
     }
 
@@ -396,89 +330,20 @@ class BucketsTableGpu {
 
     struct DeviceTableView {
         Bucket* d_buckets;
-        SpinLock* d_locks;
         curandState* d_rand_states;
         size_t* d_numOccupied;
 
-        __device__ bool tryInsertAtBucketWarp(size_t bucketIdx, TagType tag) {
-            unsigned int lane_id = threadIdx.x & 31;
-            bool success = false;
-
-            if (lane_id == 0) {
-                d_locks[bucketIdx].lock();
-            }
-            __syncwarp();
-
-            int slot = d_buckets[bucketIdx].findEmptySlotWarp();
-
-            if (slot != -1) {
-                if (lane_id == slot) {
-                    d_buckets[bucketIdx].insertAt(slot, tag);
+        __device__ bool tryInsertAtomically(size_t bucketIdx, TagType tag) {
+            for (int i = 0; i < bucketSize; ++i) {
+                if (atomicCAS(&d_buckets[bucketIdx].tags[i], EMPTY, tag) ==
+                    EMPTY) {
                     atomicAdd(
                         reinterpret_cast<unsigned long long*>(d_numOccupied),
                         1ULL
                     );
-                    success = true;
-                }
-            }
-
-            bool was_successful = (__ballot_sync(0xffffffff, success) != 0);
-
-            if (lane_id == 0) {
-                d_locks[bucketIdx].unlock();
-            }
-            __syncwarp();
-
-            return was_successful;
-        }
-
-        __device__ bool tryInsertAtBucket(size_t bucketIdx, TagType tag) {
-            d_locks[bucketIdx].lock();
-
-            int slot = d_buckets[bucketIdx].findEmptySlot();
-            if (slot != -1) {
-                d_buckets[bucketIdx].insertAt(slot, tag);
-                atomicAdd(
-                    reinterpret_cast<unsigned long long*>(d_numOccupied), 1ULL
-                );
-                d_locks[bucketIdx].unlock();
-                return true;
-            }
-            d_locks[bucketIdx].unlock();
-            return false;
-        }
-
-        __device__ bool insertWithEvictionWarp(TagType fp, size_t startBucket) {
-            TagType currentFp = fp;
-            size_t currentBucket = startBucket;
-            unsigned int lane_id = threadIdx.x & 31;
-
-            for (size_t evictions = 0; evictions < maxProbes; ++evictions) {
-                if (tryInsertAtBucketWarp(currentBucket, currentFp)) {
                     return true;
                 }
-
-                TagType evictedFp = 0;
-                if (lane_id == 0) {
-                    d_locks[currentBucket].lock();
-
-                    curandState* state = &d_rand_states[currentBucket];
-                    auto evictSlot =
-                        static_cast<size_t>(curand_uniform(state) * bucketSize);
-
-                    evictedFp = d_buckets[currentBucket].getTagAt(evictSlot);
-                    d_buckets[currentBucket].insertAt(evictSlot, currentFp);
-
-                    d_locks[currentBucket].unlock();
-                }
-
-                currentFp = __shfl_sync(0xffffffff, evictedFp, 0);
-
-                currentBucket = BucketsTableGpu::getAlternateBucket(
-                    currentBucket, currentFp
-                );
             }
-
             return false;
         }
 
@@ -487,27 +352,17 @@ class BucketsTableGpu {
             size_t currentBucket = startBucket;
 
             for (size_t evictions = 0; evictions < maxProbes; ++evictions) {
-                d_locks[currentBucket].lock();
-
-                int slot = d_buckets[currentBucket].findEmptySlot();
-                if (slot != -1) {
-                    d_buckets[currentBucket].insertAt(slot, currentFp);
-                    atomicAdd(
-                        reinterpret_cast<unsigned long long*>(d_numOccupied),
-                        1ULL
-                    );
-                    d_locks[currentBucket].unlock();
+                if (tryInsertAtomically(currentBucket, currentFp)) {
                     return true;
                 }
 
                 curandState* state = &d_rand_states[currentBucket];
                 auto evictSlot =
                     static_cast<size_t>(curand_uniform(state) * bucketSize);
-                TagType evictedFp =
-                    d_buckets[currentBucket].getTagAt(evictSlot);
-                d_buckets[currentBucket].insertAt(evictSlot, currentFp);
 
-                d_locks[currentBucket].unlock();
+                TagType evictedFp = atomicExch(
+                    &d_buckets[currentBucket].tags[evictSlot], currentFp
+                );
 
                 currentFp = evictedFp;
                 currentBucket = BucketsTableGpu::getAlternateBucket(
@@ -517,20 +372,13 @@ class BucketsTableGpu {
             return false;
         }
 
-        __device__ bool insertWarp(const T& key) {
-            auto [h1, h2, fp] = BucketsTableGpu::getCandidateBuckets(key);
-            if (tryInsertAtBucketWarp(h1, fp) ||
-                tryInsertAtBucketWarp(h2, fp)) {
-                return true;
-            }
-            return insertWithEvictionWarp(fp, h1);
-        }
-
         __device__ bool insert(const T& key) {
             auto [h1, h2, fp] = BucketsTableGpu::getCandidateBuckets(key);
-            if (tryInsertAtBucket(h1, fp) || tryInsertAtBucket(h2, fp)) {
+
+            if (tryInsertAtomically(h1, fp) || tryInsertAtomically(h2, fp)) {
                 return true;
             }
+
             return insertWithEviction(fp, h1);
         }
 
@@ -539,17 +387,10 @@ class BucketsTableGpu {
             return d_buckets[h1].containsWarp(fp) ||
                    d_buckets[h2].containsWarp(fp);
         }
-
-        __device__ bool contains(const T& key) const {
-            auto [h1, h2, fp] = BucketsTableGpu::getCandidateBuckets(key);
-            return d_buckets[h1].contains(fp) || d_buckets[h2].contains(fp);
-        }
     };
 
     DeviceTableView get_device_view() {
-        return DeviceTableView{
-            d_buckets, d_locks, d_rand_states, d_numOccupied
-        };
+        return DeviceTableView{d_buckets, d_rand_states, d_numOccupied};
     }
 };
 
@@ -573,12 +414,7 @@ __global__ void insertKernel(
 ) {
     size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx < n) {
-        // table_view.insert(keys[idx]);
-        if (blockSize & 31 == 0) {
-            table_view.insertWarp(keys[idx]);
-        } else {
-            table_view.insert(keys[idx]);
-        }
+        table_view.insert(keys[idx]);
     }
 }
 
@@ -603,10 +439,6 @@ __global__ void containsKernel(
 ) {
     size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx < n) {
-        if (blockSize & 31 == 0) {
-            output[idx] = table_view.containsWarp(keys[idx]);
-        } else {
-            output[idx] = table_view.contains(keys[idx]);
-        }
+        output[idx] = table_view.containsWarp(keys[idx]);
     }
 }
